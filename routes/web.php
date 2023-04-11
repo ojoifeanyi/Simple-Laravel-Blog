07c7147e9f5c0b5d2d0ff30d58e6e17fb06fdb55
Ojo Ifeanyi <ojoifeanyi@gmail.com>
2023-04-11 06:17:57 +0100

    added a redirect to blog page

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+   // return view('welcome');
+    return redirect()->route('blog.index');
 });
 
 Route::get('/dashboard', function () {
